478ca928bc44ce82069134868cc492e64fe9e09d
MaxTarrach <tarrachmaximilian.97@gmail.com>
2023-02-22 14:35:59 +0100

    Organized Files and woring on labels

--- Metabolomics JS Code/index.js
+++ Metabolomics JS Code/index.js
@@ -1,4 +1,4 @@
-// Author: David Kosel  
+// Author: David Kosel & Maximilian Tarrach
 // Metabolomics is the scientific study of chemical processes involving metabolites
 
 
@@ -8,7 +8,7 @@ let mouseX;
 let mouseY;
 let toolTipActivated = false; 
 var slider = document.getElementById("myRange");
-var dataSlider = document.getElementById("dataRange");
+// var dataSlider = document.getElementById("dataRange");
 var output = document.getElementById("value");
 var sliderValue = 1;
 var filteredCSdataSet =[]; 
@@ -24,6 +24,10 @@ let dataBaseHidden = false;
 //var data; 
 var TMapData = data;
 var ChemicalSpaceDataBackUp = data;
+
+let limit = 0; 
+
+let subNames = [ "Spectral Match to Baicalin from NIST14", "Spectral Match to 3-Hydroxy-4-methoxycinnamic acid from NIST14", "Massbank:PR301031 Harmane","Spectral Match to Baicalin from NIST14", "Pesticide3_Chlorotoluron_C10H13ClN2O_Urea, N'-(3-chloro-4-methylphenyl)-N,N-dimethyl-", "NCGC00380776-01_C16H24O7_"];
 
 
 var filteredTmapData = {
@@ -60,10 +64,7 @@ Chemical_Space_tree: {
 };
 
 function updateSigmaGraph(){
-  var colR;
-  var colG;
-  var colB
-  var colorBrightness = 0.6;
+
   graph.clear(); 
 
   for(var i = 0; i < dataPoints.length; i++){
@@ -72,19 +73,15 @@ function updateSigmaGraph(){
      
      if(dataBaseHidden == false){
 
-      var red = Math.round(dataPoints[i].r / 1.5);
-      var green = Math.round(dataPoints[i].g / 1.5);
-      var blue = Math.round(dataPoints[i].b / 1.5);
-
       //graph.addNode([i], { x:dataPoints[i].x , y: dataPoints[i].y, size: 2, label: dataPoints[i].getCompoundName(), color: "rgb("+dataPoints[i].r+","+dataPoints[i].g+","+ dataPoints[i].b+")" });
-      graph.addNode([i], { x:dataPoints[i].x , y: dataPoints[i].y, size: 2, color: "rgb("+red+","+green+","+blue+")"});
+      graph.addNode([i], { x:dataPoints[i].x , y: dataPoints[i].y, size: 2, color: "rgb(100,100,100)"});
     }else{
-       //Darker Colors for "hidden" Objects (chemical data space) 
-      colR = Math.floor(dataPoints[i].r * colorBrightness);
-      colG = Math.floor(dataPoints[i].g * colorBrightness);
-      colB = Math.floor(dataPoints[i].b * colorBrightness);
- 
-      graph.addNode([i], { x:dataPoints[i].x , y: dataPoints[i].y , size: 2, label: dataPoints[i].getCompoundName(), color: "rgb("+colR+","+colG+","+ colB +")" });
+     
+      var red = Math.round(dataPoints[i].r / 2);
+      var green = Math.round(dataPoints[i].g / 2);
+      var blue = Math.round(dataPoints[i].b / 2);
+
+      graph.addNode([i], { x:dataPoints[i].x , y: dataPoints[i].y , size: 2, label: dataPoints[i].getCompoundName(), color: "rgb("+red+","+green+","+ blue +")" });
      }
 
      //Highlight all DataSamples that are in the chemical space: 
@@ -95,16 +92,15 @@ function updateSigmaGraph(){
       //Class-Colors
       graph.addNode([i], { x:dataPoints[i].x , y: dataPoints[i].y, size: 10, label: dataPoints[i].getCompoundName(), color: "rgb("+dataPoints[i].r+","+dataPoints[i].g+","+ dataPoints[i].b +")" });
       
+      
+
       }
-
-
 
       // Heatmap colorization = Gradient and not just 4 colors 
 
       if(heatMapActive == true){
         //heatmap colorization
-      
-        
+         
        // get the max, min SmilesCount value
         var smilesCounts = filteredAndFoundDataPoints.map(object => { return object.SMILEScount; });
         console.log(smilesCounts);
@@ -139,9 +135,6 @@ function updateSigmaGraph(){
       }
     }
   }
-
-
-
 
 }
 var heatMapActive = false; 
@@ -223,11 +216,22 @@ var graph = new graphology.Graph();
 const {UndirectedGraph, DirectedGraph} = graphology;
 
 
-const renderer = new Sigma(graph, container, {
+s = new Sigma(graph, container, {
  
      allowInvalidContainer: true 
 
 });
+
+/*
+console.log(s);
+
+s.bind('clickNode', function(e) {
+    console.log('HEYOHEYO'); 
+}); 
+*/ 
+
+
+
 
 
 const uploadconfirm = document.getElementById('uploadconfirm').
@@ -284,8 +288,6 @@ function convertToJSON(fileNumber){
            updateSigmaGraph();
            visualizeSunburst(); 
            renderLegend(); 
-
-           createSlider();
  
       }
        
@@ -323,9 +325,23 @@ let zoomAmount;
 // An dieser Stelle den Code ändern zu: Toggle Signal interpretieren 
 
 document.getElementById("switch").onclick = function(){
-  showHeatMap();
+  
+  if (heatMapActive == false) {
+
+    showHeatMap();
+
+  }
+  
+  else {
+
+  }
+
+
  // cameraZoom = document.getElementById("zoomAmount").value;
 }
+
+
+
 //document.getElementById("centerView").onclick = function(){
 // cameraOffset.x = 0;
 // cameraOffset.y = 0;
@@ -778,31 +794,13 @@ if(dataLoaded == true){
 
   // scatterPoints();
   updateSigmaGraph();
-    // Refresh rendering:
-    renderer.refresh();
+  // Refresh rendering:
+  s.refresh();
    
   }
 }
 })
 
-dataSlider.oninput = function() {
-  output.innerHTML = this.value;
-}
-var dataSliderValue; 
-
-dataSlider.addEventListener("input", function(){
-  dataSliderValue = dataSlider.value;
-  var color = "linear-gradient(90deg, rgb(117,252,117)" + dataSliderValue + "%, rgb(214,214,214)" + dataSliderValue;
-  dataSlider.style.background = color;
- // dataSlider.style.content = document.getElementById('uploadfile').files.length;
-
-
-  console.log("DataSlider Value: "+ dataSliderValue);
-
-  heatMapActive = false;
-  convertToJSON(dataSliderValue);
-
-})
 
 var filteredAndFoundCSDataSet; 
 
@@ -834,7 +832,8 @@ let stroke = "black";
  dataPoints[i] = new DataPoint(TMapData.Chemical_Space.x[i],TMapData.Chemical_Space.y[i],dataPointSize, TMapData.Chemical_Space.colors[0].r[i], TMapData.Chemical_Space.colors[0].g[i], TMapData.Chemical_Space.colors[0].b[i],stroke, lineWidth, isFiltered);
     
  dataPoints[i].setSmiles(TMapData.Chemical_Space.labels[i]);
- dataPoints[i].setCompoundName("["+[i]+ "] " + TMapData.Chemical_Space.labels[i]);
+ //dataPoints[i].setCompoundName("["+[i]+ "] " + TMapData.Chemical_Space.labels[i]);
+ dataPoints[i].setCompoundName("["+[i]+ "] " + subNames[Math.floor(Math.random()*subNames.length)]);
 
  if(heatMapActive == false){
   dataPoints[i].setFileName("File: " + fileNumber);
@@ -1059,217 +1058,68 @@ document.getElementById("defaultOpen").click();
 //===== TABS Code END ======
 
 
-//===== Alignment Chart Code ======
-
-var xValue = [];
-var yValue = [];
-
-var trace1 = {
-  x: xValue,
-  y: yValue,
-  type: 'bar',
-  text: yValue,
-  textposition: 'auto',
-  hoverinfo: 'none',
-  marker: {
-    color: 'rgb(158,202,225)',
-    opacity: 0.6,
-    line: {
-      color: 'rgb(8,48,107)',
-      width: 1.5
-    }
-  }
-};
-
-var plotlyData = [trace1];
-
-var plotlyLayout = {
-  title: 'Alignment Chart',
-  barmode: 'stack'
-};
-
-document.getElementById("ShowGraphButton").onclick = function(){
-updateJSChartContent(); 
-generateTable(); 
-}
-
-
-
-//===== Alignment Chart Code END ======
-
-
-// ============= Chart JS Test Code ======
-
-var chartJSObject = document.getElementById("chartJS");
-
-
-var chartLabels = [""]; 
-var chartCountData  = []; 
-
-var JSChart = new Chart(chartJSObject, {
-  type: "bar",
-  data: {
-    labels: chartLabels,
-    datasets: [{
-      label: "Counted Dataset",
-      backgroundColor: 'rgba(65,105,225,0.4)',
-      barPercentage: 1,
-      data: chartCountData, 
-
-    },
-  ],
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }],
-
-    },
-    maintainAspectRatio: false,
-  }
-
-
-});
-function sortDataPointsByCount(){
-//Sort Data descending from highest count to lowest SmileCount 
-let sortedDataPoints = filteredAndFoundDataPoints.sort(
-  (p1, p2) => (p1.getSmilesCount() < p2.getSmilesCount()) ? 1 : (p1.getSmilesCount() > p2.getSmilesCount()) ? -1 : 0);
-filteredAndFoundDataPoints = sortedDataPoints; 
-}
-
-function updateJSChartContent(){
-  
-  //sorting isnt necessary, may delete
-  sortDataPointsByCount(); 
-
-  //get the Smiles+count Data Object and map it into an array
-  let JSChartData = Object.keys(countedSmiles).map((key) => [String(key), countedSmiles[key]]);
-
-// Sort this Array by Smiles Count
-  let sortedJSChartData = JSChartData.sort(
-    (p1, p2) => (p1[1] < p2[1]) ? 1 : (p1[1] > p2[1]) ? -1 : 0);
-
-console.log(sortedJSChartData);
-// Fill X and Y-Values for the Chart 
-  for(var i = 0; i<sortedJSChartData.length;i++){
-    chartLabels.push(sortedJSChartData[i][0]);
-    chartCountData.push(sortedJSChartData[i][1]);
-  }
-
-
-
-console.log(countedSmiles);
-console.log(Object.keys(countedSmiles));
-console.log(Object.values(countedSmiles));  
-  JSChart.update(); 
-}
-
-let tbl = document.createElement("table");
-let tblBody = document.createElement("tbody");
-
-function generateTable() {
-
-  // creates a <table> element and a <tbody> element
-  let tbl = document.createElement("table");
-  let tblBody = document.createElement("tbody");
-
-  // creating all cells
-  for (let i = 0; i < 5; i++) {
-    // creates a table row
-    const row = document.createElement("tr");
-
-    for (let j = 0; j < Object.keys(countedSmiles).length; j++) {
-      // Create a <td> element and a text node, make the text
-      // node the contents of the <td>, and put the <td> at
-      // the end of the table row
-      const cell = document.createElement("td");
-     cell.fitWidth = "25px";
-      const cellText = document.createTextNode(chartCountData[j]);
-      cell.appendChild(cellText);
-      row.appendChild(cell);
-    }
-
-    // add the row to the end of the table body
-    tblBody.appendChild(row);
-  }
-
-  // put the <tbody> in the <table>
-  tbl.appendChild(tblBody);
-  // appends <table> into <body>
-  document.body.appendChild(tbl);
-  // sets the border attribute of tbl to '2'
-  tbl.setAttribute("border", "2");
-  tbl.style.marginTop = "50px";
-}
-
-// Set Range Slider
-let minCountRange = document.getElementById("minCount");
-let maxCountRange = document.getElementById("maxCount");
-
-
-document.getElementById("countRangeButton").onclick = function(){
- 
-  setChartJSRange();
-  generateTable(); 
- 
-}
-
-function setChartJSRange(){
-  for(var i = 0; i<chartCountData.length; i++){
-
-    if(chartCountData[i] > maxCountRange.value){
-      chartCountData.splice(i, 1);
-      chartLabels.splice(i,1);
-    }
-
-    if(chartCountData[i] < minCountRange.value){
-      console.log("reached"); 
-      chartCountData.splice(i, 1);
-     chartLabels.splice(i,1);
-    }
-    
-    JSChart.update();
-
-}
-
-
-console.log("Min:"+minCountRange.value+" Max:"+maxCountRange.value);
-console.log(chartCountData);
-console.log("chart updated");
-}
-
-
-
-function createSlider(){
-
-
-  var maxFile = dataCollector.length; 
-
-  var slider = document.getElementById('centerSlider');
-
-  var x = document.createElement('INPUT');
-
-  x.setAttribute("type", "range");
-  x.setAttribute("min", "0");
-  x.setAttribute("max", "5");
-  x.setAttribute("value", "0");
-
-  slider.appendChild(x);
-
-}
-
+// ========= File Navigation in Tmap START ==========
 
 const decrementButton = document.querySelector("#decrement");
 const incrementButton = document.querySelector("#increment");
 const incdecCount = document.querySelector("#incdecCount");
 
+
 decrementButton.addEventListener("click", () => {
-  incdecCount.innerText = parseInt(incdecCount.innerText) - 1;
+
+  if (document.getElementById("incdecInput").value == 0) {
+
+    console.log('Hey');
+
+    incdecCount.innerText = parseInt(incdecCount.innerText);
+
+    document.getElementById("incdecInput").value = 0;
+
+    convertToJSON(incdecCount.innerText);
+
+    heatMapActive = false; 
+
+  }
+
+  else {
+
+    console.log('Ho');
+
+    document.getElementById("incdecInput").value = document.getElementById("incdecInput").value - 1;
+ 
+    convertToJSON(document.getElementById("incdecInput").value); 
+
+    heatMapActive = false; 
+
+  };
+
 });
 
 incrementButton.addEventListener("click", () => {
-  incdecCount.innerText = parseInt(incdecCount.innerText) + 1;
+
+  if (document.getElementById("incdecInput").value == 3) {
+
+
+    document.getElementById("incdecInput").value = document.getElementById("incdecInput").value;
+
+
+    convertToJSON(document.getElementById("incdecInput").value);
+
+    heatMapActive = false; 
+
+  }
+
+  else {
+
+    document.getElementById("incdecInput").value = parseInt(document.getElementById("incdecInput").value) + 1;
+
+
+    convertToJSON(parseInt(document.getElementById("incdecInput").value));
+
+    heatMapActive = false; 
+
+  }
+  
 });
+
+// ========= File Navigation in Tmap END ==========
